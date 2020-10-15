e9d75b03e25970d05f42e412973902d4c3f6e455
Sanskar Jaiswal <jaiswalsanskar078@gmail.com>
2020-10-15 12:33:56 +0530

    Change access_type to offline

--- enigma7_backend/settings.py
+++ enigma7_backend/settings.py
@@ -134,7 +134,7 @@ SOCIALACCOUNT_PROVIDERS = {
             'email',
         ],
         'AUTH_PARAMS': {
-            'access_type': 'online',
+            'access_type': 'offline',
         }
     }
 }
